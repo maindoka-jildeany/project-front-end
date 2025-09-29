22385e215c107227f2d5d8fa659ae395ff9b2d88
maindoka-jildeany <12410495@student.unklab.ac.id>
2025-09-29 21:40:08 +0800

    tambah type,task  item dan list

---
+++ mid front end/src/component/TaskItem.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Task } from "../types";
+
+interface TaskItemProps {
+  task: Task;
+  onToggle: (id: number) => void;
+  children?: React.ReactNode; // untuk tombol tambahan (ex: Delete)
+}
+
+const TaskItem: React.FC<TaskItemProps> = ({ task, onToggle, children }) => {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "space-between", 
+      marginBottom: "8px",
+      padding: "8px",
+      border: "1px solid #ccc",
+      borderRadius: "8px"
+    }}>
+      <span
+        onClick={() => onToggle(task.id)}
+        style={{
+          textDecoration: task.status === "done" ? "line-through" : "none",
+          cursor: "pointer",
+        }}
+      >
+        {task.title}
+      </span>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default TaskItem;
---
+++ mid front end/src/component/TaskList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Task } from "../types";
+import TaskItem from "./TaskItem";
+
+interface TaskListProps {
+  tasks: Task[];
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, onToggle, onDelete }) => {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem key={task.id} task={task} onToggle={onToggle}>
+          <button onClick={() => onDelete(task.id)}>Delete</button>
+        </TaskItem>
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
